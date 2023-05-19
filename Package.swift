11f936d26fb5a6862d142feacf5a4a9d50327587
Mike Thornton <mthornt@icloud.com>
2023-05-19 09:30:14 -0700

    remove commented code

--- Package.swift
+++ Package.swift
@@ -7,21 +7,20 @@ let package = Package(
     name: "thornton-swift-tools",
     platforms: [
         .macOS("12.0")
-//        .macOS("13.3.1")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-//        .library(name: "thornton-swift-tools", targets: ["thornton-swift-tools"]),
+        //.library(name: "thornton-swift-tools", targets: ["thornton-swift-tools"]),
         .library(name: "IconLibrary", targets: ["IconLibrary"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-//        .package(url: "https://github.com/apple/swift-syntax.git", from: "508.0.0"),
+        //.package(url: "https://github.com/apple/swift-syntax.git", from: "508.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(name: "thornton-swift-tools", dependencies: []),
+        //.target(name: "thornton-swift-tools", dependencies: []),
         
         // MARK: - Plugin : Generate Contributors
         .plugin(
@@ -69,12 +68,5 @@ let package = Package(
             dependencies: ["AssetConstantsExec"]
         ),
         .executableTarget(name: "AssetConstantsExec"),
-        
-//        .executableTarget(
-//            name: "AddCodingKeysExecutable",
-//            dependencies: [
-//                .product(name: "SwiftSyntax", package: "swift-syntax"),
-//            ]
-//        ),
     ]
 )
